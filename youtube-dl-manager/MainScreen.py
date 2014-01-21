8b58596de5cab4f7b54add31a1a11022526084ac
Henri Verroken <henriverroken@gmail.com>
2014-01-21 18:41:42 +0100

    Moved initialization code and added queuebox prototype

--- youtube-dl-manager/MainScreen.py
+++ youtube-dl-manager/MainScreen.py
@@ -1,10 +1,16 @@
 import curses
 
 from Screen import Screen
+from QueueBox import QueueBox
 
 class MainScreen(Screen):
+    def initialize(self):
+        self.queueBox = QueueBox(self, (2, 3), (8, 50))
+        self.addChild(self.queueBox)
+    
     # Drawing
     def display(self):
+        self.size = self.stdscr.getmaxyx()
         self.__drawBox()
 
     def __drawBox(self):
@@ -13,4 +19,5 @@ class MainScreen(Screen):
         title = "Youtube DL Manager"
         y, x = self.abs(0,3)
         self.addstr(y, x, title, curses.A_BOLD)
+
     
