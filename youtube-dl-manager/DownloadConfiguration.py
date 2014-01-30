2f851790eeae83c9d3b61a2ae25b451827e4b3f5
Henri Verroken <henriverroken@gmail.com>
2014-01-30 14:58:52 +0100

    Added DownloadConfiguration model class

---
+++ youtube-dl-manager/DownloadConfiguration.py
@@ -0,0 +1,7 @@
+
+class DownloadConfiguration(object):
+    def __init__(self, mediaObject, mediaFormat, filename):
+        self.mediaObject = mediaObject
+        self.mediaFormat = mediaFormat
+        self.filename = filename
+        
