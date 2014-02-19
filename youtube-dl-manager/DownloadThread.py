fe390fbb40c79013877051c4c36dd2c86dd7126c
Henri Verroken <henriverroken@gmail.com>
2014-02-19 19:28:51 +0100

    Added dummy DownloadThread for downloading in the background

---
+++ youtube-dl-manager/DownloadThread.py
@@ -0,0 +1,30 @@
+
+import time
+from threading import Thread
+
+from Notification import Notification
+
+class DownloadThread(Thread):
+
+    # This notification is posted when the thread is finished
+    DONE_NOTIFICATION = "DownloadThreadDoneNotification"
+    def __init__(self, downloadConfiguration):
+        """Initializes a download thread.
+
+        downloadConfiguration - The object to be downloaded. Must be
+            an instance of DownloadConfiguration.
+        doneHandler - A callable that will be called from this thread
+            when the download has finished. It must accept exactly one
+            argument. This will be None in case of success, or an
+            Exception when one occurs.
+        """
+        self.downloadConfiguration = downloadConfiguration
+
+    def run(self):
+        time.sleep(3)
+        self.__notifyDone(None)
+        pass
+
+    def __notifyDone(self, e):
+        n = Notification(self.DONE_NOTIFICATION, self)
+        Notification.post(n)
